0e296708c33df2fbd972b985a718ab408b41976e
unknown <leonardo.1117@hotmail.com>
2020-10-16 08:07:16 -0300

    add credit card section on main screen

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -14,9 +14,29 @@ export default function Main() {
         <View style={styles.header}>
           <Text style={styles.headerText}>Olá, Leonardo</Text>
           <View style={styles.headerIcons}>
-            <Feather name="eye-off" color="#fff" size={24} style={styles.eyeIcon}/>
-            <Feather name="settings" color="#fff"size={24} style={styles.gearIcon}/>
+            <Feather name="eye-off" color="#fff" size={24} style={styles.eyeIcon} />
+            <Feather name="settings" color="#fff" size={24} style={styles.gearIcon} />
           </View>
+        </View>
+
+        <View style={styles.cardInvoice}>
+          <View style={styles.creditCard}>
+
+            <Feather name="credit-card" color="rgba(0,0,0, 0.6)" size={22} />
+            <Text style={styles.creditCardText}>Cartão de Crédito</Text>
+
+          </View>
+
+          <View style={styles.currentBill}>
+            <Text style={styles.billText}>Fatura Atual</Text>
+            <Text style={styles.billValue}>R$ 146,54</Text>
+          </View>
+
+          <View style={styles.availableCredit}>
+            <Text style={styles.availableCreditText}>Limite Disponível</Text>
+            <Text style={styles.availableCreditValue}>RS 1.844,16</Text>
+          </View>
+
 
         </View>
 
@@ -40,6 +60,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
 
     flexDirection: 'row',
+    marginBottom: 10,
   },
   headerText: {
     fontSize: 32,
@@ -60,5 +81,45 @@ const styles = StyleSheet.create({
     padding: 10,
     backgroundColor: "rgba(255, 255, 255, 0.1)",
     borderRadius: 50,
+  },
+  cardInvoice: {
+    padding: 20,
+    marginVertical: 5,
+    backgroundColor: '#fff',
+    borderRadius: 4,
+    marginTop: 15,
+  },
+  creditCard: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  creditCardText: {
+    color: 'rgba(0,0,0, 0.6)',
+    marginLeft: 20,
+    fontSize: 16
+  },
+  currentBill: {
+
+  },
+  billText: {
+    color: 'rgba(0,0,0, 0.6)',
+    marginVertical: 10,
+  },
+  billValue: {
+    color: '#05B8C4',
+    fontWeight: 'bold',
+    fontSize: 22,
+  },
+  availableCredit: {
+    flexDirection: 'row',
+    marginTop: 5,
+  },
+  availableCreditText: {
+    color: 'rgba(0,0,0, 0.6)',
+    marginRight: 5,
+  },
+  availableCreditValue: {
+    fontWeight: 'bold',
+    color: 'green',
   }
 });
